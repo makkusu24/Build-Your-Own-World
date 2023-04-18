8363f6b71118d1a7ff2aa34875d448184909fb75
Max Boston <maxboston@berkeley.edu>
2023-04-17 21:24:57 -0700

    avatar move keyboard, but not on input string

--- proj3/byow/Core/Point.java
+++ proj3/byow/Core/Point.java
@@ -19,6 +19,14 @@ public class Point {
         return y;
     }
 
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
     @Override
     public boolean equals(Object other) {
         if (this == other) {
